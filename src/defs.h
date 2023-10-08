8a4b8939e3a4ae2026cf118970a611c88aa6258a
Joachim Wiberg <troglobit@gmail.com>
2023-10-08 09:04:13 +0200

    Drop obsolete macros and update src/defs.h accordingly
    All current systems have time.h, no need to check for it.  Not all
    systems have sys/time.h, though, but those that do allow including
    both files.  The configure script already checks for sys/time.h.
    
    Found by autoheader.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/defs.h
+++ src/defs.h
@@ -52,16 +52,10 @@
 #include <sys/sockio.h>
 #endif
 
-#if TIME_WITH_SYS_TIME
+#if HAVE_SYS_TIME_H
 # include <sys/time.h>
-# include <time.h>
-#else
-# if HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
 #endif
+#include <time.h>
 
 #include <limits.h>
 
