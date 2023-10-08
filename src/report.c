fec7d84a1b0f3e6c150a6fa9253e7248589e7809
Joachim Wiberg <troglobit@gmail.com>
2023-10-08 13:12:43 +0200

    Move license information to top of file, as is customary
    CMU have no particular restrictions that require mentioning in the top
    LICENSE file for the project.
    
    Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

--- src/report.c
+++ src/report.c
@@ -1,14 +1,23 @@
-/* Routines associated with reporting/logging messages:
-	report_init()
-	report()
-	close_and_reopen_log_file()
-	get_errmsg()
-*/
-
-
-/*	This is based on report.c from Carnegie Mellon University (CMU) dhcpd 3.3.7.
-	As a result, it is subject to copyright, see the COPYING file for details.
-*/
+/*
+ * Copyright (c) 1996 by Carnegie Mellon University
+ * All Rights Reserved
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose and without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in
+ * supporting documentation, and that the name of CMU not be
+ * used in advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ *
+ * CMU DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+ * ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
+ * CMU BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+ * ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
 
 #ifdef HAVE_CONFIG_H
 # include "config.h"
--- src/report.h
+++ src/report.h
@@ -1,5 +1,26 @@
-#ifndef REPORT_H
-#define REPORT_H
+/*
+ * Copyright (c) 1996 by Carnegie Mellon University
+ * All Rights Reserved
+ *
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose and without fee is hereby granted,
+ * provided that the above copyright notice appear in all copies and that
+ * both that copyright notice and this permission notice appear in
+ * supporting documentation, and that the name of CMU not be
+ * used in advertising or publicity pertaining to distribution of the
+ * software without specific, written prior permission.
+ *
+ * CMU DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
+ * ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO EVENT SHALL
+ * CMU BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR
+ * ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS,
+ * WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION,
+ * ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ */
+
+#ifndef REPORT_H_
+#define REPORT_H_
 
 extern void report_init(int dont_fork, char *logfile_name);
 extern char *get_errmsg(void);
@@ -12,4 +33,4 @@ extern void report();
 
 void close_and_reopen_log_file(char * logfile_name);
 
-#endif /* not REPORT_H */
+#endif /* REPORT_H_ */
